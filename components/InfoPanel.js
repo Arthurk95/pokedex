0e0ee030793e39e42814cdc846104796e5d87bb3
Arthurk95 <arthurnk03@gmail.com>
2020-09-07 12:11:47 -0500

    Added StatBars and Close button

--- components/InfoPanel.js
+++ components/InfoPanel.js
@@ -1,12 +1,27 @@
-import Stat from "../components/Stat";
+import Attribute from "./Attribute";
+import StatBar from "./Stats/StatBar";
+import AttributeList from "./AttributeList";
 
 export default function InfoPanel(props) {
   let info = props.data;
-  let styles = props.active ? "info-panel active" : "info-panel";
+  let styles = props.show ? "info-panel active" : "info-panel";
+  let statBars = info.stats.map((stat, index) => {
+    return (
+      <StatBar
+        color="var(--Accent4)"
+        stat={stat}
+        secondaryColor="#EDEDED"
+        key={`${info.id}-${stat.stat.name}`}
+      />
+    );
+  });
+
   return (
     <div className={styles}>
-      <Stat title="Height" value={toFeetInches(info.height * 4)} />
-      <Stat title="Category" value="" />
+      <AttributeList>
+        <Attribute title="Height" value={toFeetInches(info.height * 4)} />
+      </AttributeList>
+      <div className="flex-centered-list">{statBars}</div>
     </div>
   );
 }
