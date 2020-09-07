43a3f942805f72fa52a3675e1c77461b9e4a2bbf
Arthurk95 <arthurnk03@gmail.com>
2020-09-07 18:13:37 -0500

    Removed title element

--- components/Attributes/AttributeList.js
+++ components/Attributes/AttributeList.js
@@ -1,13 +1,10 @@
 import styles from "../../styles/Attributes.module.css";
-import Title from "../Title";
 import Attribute from "./Attribute";
 
 export default function AttributeList(props) {
   let info = props.data;
   return (
     <div className={styles["attribute-list"]}>
-      <Title additionalStyles={{ sizeMultiplier: 1.5 }} text="Attributes" />
-
       <Attribute title="Height" value={toFeetInches(info.height * 4)} />
     </div>
   );
