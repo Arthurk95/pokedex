a3ed3e8e072c418c8f2646b6944740ceeecac2bf
Arthurk95 <arthurnk03@gmail.com>
2020-09-06 19:08:00 -0500

    Added infinite scroll package, changed Home() to a class structure with state

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,68 @@ import styles from "../styles/Home.module.css";
 import axios from "axios";
 import ListLayout from "../components/ListLayout";
 import PokemonCard from "../components/PokemonCard";
+import InfiniteScroll from "react-infinite-scroller";
 
-const data = JSON.parse(
-  '{"data":{"count":1050,"next":"https://pokeapi.co/api/v2/pokemon?offset=20&limit=20","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"},{"name":"charmander","url":"https://pokeapi.co/api/v2/pokemon/4/"},{"name":"charmeleon","url":"https://pokeapi.co/api/v2/pokemon/5/"},{"name":"charizard","url":"https://pokeapi.co/api/v2/pokemon/6/"},{"name":"squirtle","url":"https://pokeapi.co/api/v2/pokemon/7/"},{"name":"wartortle","url":"https://pokeapi.co/api/v2/pokemon/8/"},{"name":"blastoise","url":"https://pokeapi.co/api/v2/pokemon/9/"},{"name":"caterpie","url":"https://pokeapi.co/api/v2/pokemon/10/"},{"name":"metapod","url":"https://pokeapi.co/api/v2/pokemon/11/"},{"name":"butterfree","url":"https://pokeapi.co/api/v2/pokemon/12/"},{"name":"weedle","url":"https://pokeapi.co/api/v2/pokemon/13/"},{"name":"kakuna","url":"https://pokeapi.co/api/v2/pokemon/14/"},{"name":"beedrill","url":"https://pokeapi.co/api/v2/pokemon/15/"},{"name":"pidgey","url":"https://pokeapi.co/api/v2/pokemon/16/"},{"name":"pidgeotto","url":"https://pokeapi.co/api/v2/pokemon/17/"},{"name":"pidgeot","url":"https://pokeapi.co/api/v2/pokemon/18/"},{"name":"rattata","url":"https://pokeapi.co/api/v2/pokemon/19/"},{"name":"raticate","url":"https://pokeapi.co/api/v2/pokemon/20/"}]},"status":200,"statusText":"","headers":{"cache-control":"public, max-age=86400, s-maxage=86400","content-type":"application/json; charset=utf-8"},"config":{"url":"https://pokeapi.co/api/v2/pokemon","method":"get","headers":{"Accept":"application/json, text/plain, */*"},"transformRequest":[null],"transformResponse":[null],"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","maxContentLength":-1,"maxBodyLength":-1},"request":{}}'
-);
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Elements: [],
+      NextLink: "https://pokeapi.co/api/v2/pokemon",
+      hasMoreItems: true,
+    };
+  }
 
-export default function Home() {
-  var pokemon = data.data.results;
+  componentDidMount() {
+    fetch(this.state.NextLink)
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          Data: result.results,
+          Elements: result.results.map((poke, index) => {
+            return <PokemonCard key={poke.id} link={poke.url}></PokemonCard>;
+          }),
+          NextLink: result.next,
+        });
+      });
+  }
 
-  var elements = pokemon.map((poke, index) => {
-    return <PokemonCard link={poke.url}></PokemonCard>;
-  });
+  loadMore = async (page) => {
+    if (this.state.NextLink === null) {
+      this.setState({
+        hasMoreItems: false,
+      });
+    } else {
+      fetch(this.state.NextLink)
+        .then((res) => res.json())
+        .then((result) => {
+          let els = this.state.Elements;
+          els.push(
+            result.results.map((poke, index) => {
+              return <PokemonCard key={poke.id} link={poke.url}></PokemonCard>;
+            })
+          );
+          this.setState({
+            Data: result.results,
+            NextLink: result.next,
+            Elements: els,
+          });
+        });
+    }
+  };
 
-  return <ListLayout>{elements}</ListLayout>;
+  render() {
+    return (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={this.loadMore.bind(this)}
+        hasMore={this.state.hasMoreItems}
+        loader={<p>Loading...</p>}
+      >
+        <div className="flex-list" key="0">
+          {this.state.Elements}
+        </div>
+      </InfiniteScroll>
+    );
+  }
 }
