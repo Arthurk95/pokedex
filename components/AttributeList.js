bdc1bbb57f76b97ce2a03613b38515dc2b5ced5d
Arthurk95 <arthurnk03@gmail.com>
2020-09-07 12:19:02 -0500

    Added title to the list

--- components/AttributeList.js
+++ components/AttributeList.js
@@ -1,5 +1,11 @@
 import styles from "../styles/Attributes.module.css";
+import Title from "./Title";
 
 export default function AttributeList({ props, children }) {
-  return <div className={styles["attribute-list"]}>{children}</div>;
+  return (
+    <div className={styles["attribute-list"]}>
+      <Title additionalStyles={{sizeMultiplier: 1.5}} text="Attributes" />
+      {children}
+    </div>
+  );
 }
