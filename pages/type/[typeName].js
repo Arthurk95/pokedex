e30eb2f3c816c8261b3a2eec397c8a2d63bcacda
Arthurk95 <arthurnk03@gmail.com>
2020-09-07 18:43:26 -0500

    Added proper useEffect method to make API fetch

--- pages/type/[typeName].js
+++ pages/type/[typeName].js
@@ -2,17 +2,22 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Type() {
+  const [data, setData] = useState();
   const router = useRouter();
   const { typeName } = router.query;
-  const [data, setData] = useState();
 
+  // only trigger when typeName changes...??? i have no idea
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/type/${"ice"}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
+    if (typeName !== undefined) {
+      fetch(`https://pokeapi.co/api/v2/type/${typeName}`)
+        .then((res) => res.json())
+        .then((data) => {
+          // typeData = data;
+          setData(data);
+        });
+    }
+  }, [typeName]);
 
-  return <div>{data.name}</div>;
+  console.log(data);
+  return <div></div>;
 }
