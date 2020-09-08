703eff22155f69d5cd72570381eb6d96d53595bf
Arthurk95 <arthurnk03@gmail.com>
2020-09-08 15:57:07 -0500

    Modified params to fit new changes

--- components/Stats/StatsPanel.js
+++ components/Stats/StatsPanel.js
@@ -2,19 +2,19 @@ import StatBar from "./StatBar";
 import styles from "../../styles/Stats.module.css";
 
 export default function StatsPanel(props) {
-  let { info } = props;
-  let statBars = info.stats.map((stat, index) => {
+  let { stats, id } = props;
+  let statBars = stats.map((stat, index) => {
     return (
       <StatBar
         color="var(--Accent4)"
         stat={stat}
         secondaryColor="#EDEDED"
-        key={`${info.id}-${stat.stat.name}`}
+        key={`${id}-${stat.name}`}
       />
     );
   });
 
-  let classList = props.toShow ? styles["stats-panel"] : "display-none";
+  let classList = styles["stats-panel"];
 
   return <div className={classList}>{statBars}</div>;
 }
