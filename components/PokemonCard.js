8a7096f82aded757e93e4bffd228ba220ec4002b
Arthurk95 <arthurnk03@gmail.com>
2020-09-08 15:56:14 -0500

    Completely rewrote so there's no GET requests in this component

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,89 +1,48 @@
 import InfoPanel from "../components/InfoPanel";
 import Overlay from "../components/Overlay";
 import Title from "../components/Title";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import styles from "../styles/PokeCard.module.css";
-import Types from "./Types/Types";
-import { useState, useEffect } from "react";
 import { toCapital } from "../utils/util";
-import axios from "axios";
+import Link from "next/link";
 
-function PokemonCard({ link }) {
-  const [active, setActive] = useState(false);
-  const [hover, setHover] = useState(false);
+const IMAGE_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+const IMAGE_EXT = ".png";
 
-  const [isFetching, pokeData] = useDataFetch(link);
-
-  if (isFetching) {
-    return <div>Loading...</div>;
-  }
-  let { name, sprites, types } = pokeData;
+function PokemonCard(props) {
+  let { name, id, redirect, mini, imgUrl } = props;
+  const href = `/pokemon/${name}`;
   name = toCapital(name);
-  const image = sprites.other["official-artwork"].front_default;
+  const image = imgUrl ? imgUrl : `${IMAGE_URL}${id}${IMAGE_EXT}`;
 
-  let classes = active
-    ? styles["pokemon-card"] +
-      " " +
-      styles["pokemon-card-grid"] +
-      " " +
-      styles["default-cursor"]
-    : styles["pokemon-card"];
-  return (
-    <div
-      className={classes}
-      onClick={() => setActive(true)}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
-      <p className={styles.id}>#{pokeData.id}</p>
+  let classes = styles["pokemon-card"];
+  if (!redirect) {
+    classes += " " + styles["default-cursor"];
+  }
+  if (mini) {
+    classes += " " + styles.mini;
+  }
+
+  let html = (
+    <div className={classes}>
+      <p className={styles.id}>#{id}</p>
+      <Overlay text="Click for more details" />
       <div className="flex-list-centered relative">
-        <Overlay hide={active} text="Click for more details" hover={hover} />
         <div className={styles["img-container"]}>
-          <Types types={types} />
           <img src={image} />
         </div>
-        {!active && (
-          <Title
-            additionalStyles={{ sizeMultiplier: 1.5, center: true }}
-            text={name}
-          />
-        )}
-      </div>
-
-      <InfoPanel show={active} data={pokeData} />
-      <div
-        className={`close-button ${!active ? " display-none" : ""}`}
-        onClick={setActive(false)}
-      >
-        <FontAwesomeIcon icon={faTimes} />
+        <Title
+          additionalStyles={{ sizeMultiplier: 1.5, center: true }}
+          text={name}
+        />
       </div>
     </div>
   );
-}
-
-function useDataFetch(link) {
-  const [isFetching, setFetching] = useState(false);
-  const [pokeData, setPokeData] = useState({});
-
-  useEffect(
-    function fetch() {
-      (async function () {
-        setFetching(true);
-        setPokeData(await fetchData(link));
-        setFetching(false);
-      })();
-    },
-    [link]
-  );
-
-  return [isFetching, pokeData];
-}
-
-async function fetchData(link) {
-  let res = await axios.get(link);
-  console.log(res.data);
-  return res.data;
+  if (redirect) {
+    return <Link href={href}>{html}</Link>;
+  } else {
+    return html;
+  }
 }
 
 export default PokemonCard;
