f17786adab3d65fb55ecfb505c5e4acaedd4ca7e
Arthurk95 <arthurnk03@gmail.com>
2020-09-08 16:00:33 -0500

    getImageUrl function returns a valid image URL if it exists

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,22 @@
+const IMAGE_URL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+const IMAGE_URL_BACKUP =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const IMAGE_EXT = ".png";
+
 export function toCapital(val) {
+  // console.log(val);
   val = val.charAt(0).toUpperCase() + val.slice(1);
   return val;
+}
+
+export async function getImageUrl(pokemonId) {
+  let url;
+  try {
+    url = `${IMAGE_URL}${pokemonId}${IMAGE_EXT}`;
+    const imageRes = await axios.get(url);
+  } catch (e) {
+    url = `${IMAGE_URL_BACKUP}${pokemonId}${IMAGE_EXT}`;
+  }
+  return url;
 }
