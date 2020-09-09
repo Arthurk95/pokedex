f5638ea6d81ffd3465204817bdccfa3a9b984b42
Arthurk95 <arthurnk03@gmail.com>
2020-09-09 14:36:34 -0500

    Made an array of image URLs to try if one fails

--- utils/util.js
+++ utils/util.js
@@ -1,8 +1,13 @@
-const IMAGE_URL =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
-const IMAGE_URL_BACKUP =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 const IMAGE_EXT = ".png";
+
+const IMAGE_URLS = [
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/",
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+  "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/",
+  "https://assets.pokemon.com/assets/cms2/img/pokedex/full/",
+];
+
+import axios from "axios";
 
 export function toCapital(val) {
   // console.log(val);
@@ -11,12 +16,25 @@ export function toCapital(val) {
 }
 
 export async function getImageUrl(pokemonId) {
-  let url;
-  try {
-    url = `${IMAGE_URL}${pokemonId}${IMAGE_EXT}`;
-    const imageRes = await axios.get(url);
-  } catch (e) {
-    url = `${IMAGE_URL_BACKUP}${pokemonId}${IMAGE_EXT}`;
+  async function tryLink(link) {
+    try {
+      const imageRes = await axios.get(link);
+      return true;
+    } catch {
+      return false;
+    }
   }
-  return url;
+  for (const imageUrl of IMAGE_URLS) {
+    let url = `${imageUrl}${pokemonId}${IMAGE_EXT}`;
+    if (await tryLink(url)) {
+      console.log(url);
+      return url;
+    }
+  }
+}
+
+export function getIdFromUrl(url) {
+  let id = url.split("/");
+  id = id[id.length - 2];
+  return id;
 }
