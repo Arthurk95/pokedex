e82165ed06c4a7153a5f75d1cc62252b379b3724
Arthurk95 <arthurnk03@gmail.com>
2020-09-09 14:36:14 -0500

    Added search functionality

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,76 @@
 import PokemonCard from "../components/PokemonCard";
 import InfiniteScroll from "react-infinite-scroller";
+import { AllPoke } from "../utils/allPoke";
 import axios from "axios";
 import { useState } from "react";
+import Search from "../components/Search";
+import { getIdFromUrl } from "../utils/util";
+import Loader from "../components/Loader";
 
 const API_CORE = "https://pokeapi.co/api/v2/";
 
 export default function Home(props) {
   const [nextLink, setNextLink] = useState(props.nextLink);
   const [pokeArray, setPokeArray] = useState(props.pokeList);
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchResults, setSearchResults] = useState(AllPoke);
   const [hasMore, setHasMore] = useState(true);
 
   const loadMore = async (page) => {
-    try {
-      const { pokeList, url } = await fetchPokeNameList(nextLink);
+    if (!isSearching) {
+      try {
+        const { pokeList, url } = await fetchPokeNameList(nextLink);
 
-      let newArray = pokeArray;
-      pokeList.map((n, index) => {
-        newArray.push(n);
-      });
-      console.log(newArray);
-      setNextLink(url);
-      setPokeArray(newArray);
-    } catch (err) {
-      setHasMore(false);
+        let newArray = pokeArray;
+        pokeList.map((n, index) => {
+          newArray.push(n);
+        });
+        setNextLink(url);
+        setPokeArray(newArray);
+      } catch (err) {
+        setHasMore(false);
+      }
     }
   };
 
-  let pokeElements = pokeArray.map((name, index) => {
-    return <PokemonCard name={name} id={index + 1} redirect={true} />;
+  const onSearchEntry = (e) => {
+    let val = e.target.value;
+    let oldResults = searchResults;
+    if (val !== "") {
+      let newList = [];
+
+      newList = oldResults.filter((poke) =>
+        poke.name.includes(val.toLowerCase())
+      );
+
+      setSearchResults(newList);
+      setIsSearching(true);
+      setPokeArray(newList);
+    } else {
+      setSearchResults(AllPoke);
+      setPokeArray(props.pokeList);
+      setIsSearching(false);
+    }
+  };
+
+  let pokeElements = pokeArray.map((poke) => {
+    return <PokemonCard name={poke.name} id={poke.id} redirect={true} />;
   });
+
   return (
-    <InfiniteScroll
-      pageStart={0}
-      loadMore={loadMore}
-      hasMore={hasMore}
-      loader={<p>Loading...</p>}
-    >
-      <div className="grid-list-4-cols" key="list">
-        {pokeElements}
-      </div>
-    </InfiniteScroll>
+    <div className="flex-centered-list">
+      <Search onChangeFunc={onSearchEntry} placeholderText="Search Pokemon" />
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={loadMore}
+        hasMore={hasMore}
+        loader={<Loader />}
+      >
+        <div className="grid-list four-cols" key="list">
+          {pokeElements}
+        </div>
+      </InfiniteScroll>
+    </div>
   );
 }
 
@@ -53,8 +84,21 @@ async function fetchPokeNameList(url) {
   const res = await axios.get(url);
 
   const pokeList = res.data.results.map((poke) => {
-    return poke.name;
+    return { name: poke.name, id: getIdFromUrl(poke.url) };
   });
   url = res.data.next;
   return { pokeList, url };
+}
+
+// used to fill allPoke.js file for search functionality
+async function getAllPokemonNames() {
+  const res = await axios.get(API_CORE + "pokemon?limit=2000");
+
+  let pokeList = res.data.results.map((poke) => {
+    let id = poke.url.split("/");
+    id = id[id.length - 2];
+    return { name: poke.name, id: id };
+  });
+
+  return pokeList;
 }
