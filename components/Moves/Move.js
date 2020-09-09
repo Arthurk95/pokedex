86ac6c64a5fa8f8970c176461825a9a088f9f251
Arthurk95 <arthurnk03@gmail.com>
2020-09-09 14:35:14 -0500

    Added in HTTP call to get Move data

--- components/Moves/Move.js
+++ components/Moves/Move.js
@@ -1,5 +1,54 @@
 import styles from "../../styles/Moves.module.css";
+import { useState } from "react";
 
 export default function Move({ data }) {
-  return <p className={styles.move}>{data.name}</p>;
+  const [stats, updateStats] = useState(undefined);
+  const [statsShown, toggleStats] = useState(false);
+
+  const toggleStats = async () => {
+    if (stats === undefined) {
+      await getStats();
+    }
+
+    toggleStats(!statsShown);
+  };
+
+  const getStats = async () => {
+    const res = await axios.get(data.url);
+    const { accuracy, power, pp } = res.data;
+    const damageClass = res.data.damage_class.name;
+    const description = res.data.effect_entries[0].short_effect;
+
+    updateStats({
+      accuracy,
+      power,
+      pp,
+      damageClass,
+      description,
+    });
+  };
+
+  let statsElement = "";
+  if (stats) {
+    statsElement = (
+      <div className={styles["stats-panel"]}>
+        <div className="flex-list">
+          <p>Power: {power}</p>
+          <p>Class: {damageClass}</p>
+          <p>Accuracy: {accuracy}</p>
+          <p>PP: {pp}</p>
+        </div>
+        <p>{description}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles["move-container"]}>
+      <p className={styles.move} onClick={toggleStats}>
+        {data.name}
+      </p>
+      {statsShown && statsElement}
+    </div>
+  );
 }
